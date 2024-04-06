510ea240d2f6ea6185d3156344cfa98f8a8fbcf4
Zhiyi Chen <bree_chen2005@126.com>
2024-04-06 14:32:23 +0800

    add options

--- nextjs-firebase-boilerplate/src/app/survey/page.js
+++ nextjs-firebase-boilerplate/src/app/survey/page.js
@@ -27,15 +27,16 @@ export default function Page() {
         </h2>
         <form className="my-8 bg-white rounded" onSubmit={handleSubmit}>
           <LabelInputContainer className="mb-4">
-            <Label htmlFor="email">Overall, on a scale 1 to 5 (1 =worst, 5= best) how would you rate your overall happiness today?
+            <Label htmlFor="email" >Overall, on a scale 1 to 5 (1 =worst, 5= best) how would you rate your overall happiness today?
 </Label>
-            <Input
-              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              placeholder="I want to to get rich"
-              type="text"
-              onChange={(e) => setAns1(e.target.value)}
-            />
-          </LabelInputContainer>
+</LabelInputContainer>
+            
+            <input type="radio" id="huey" name="drone" value="huey"  />
+            <label for="huey" class="blue-text">1</label>
+            <input type="radio" id="huey" name="drone" value="huey"  />
+            <label for="huey" class="blue-text">2</label>
+            
+          
           <LabelInputContainer className="mb-4">
             <Label htmlFor="email">On a scale from 1 to 5 (1 =least, 5= most) how would you rate your stress levels today?</Label>
             <Input
@@ -72,6 +73,38 @@ export default function Page() {
         <h2 className="font-bold text-xl text-[#2774AE] dark:text-[#2774AE]">
           Goals Questions
         </h2>
+
+        <LabelInputContainer className="mb-4">
+            <Label htmlFor="email">On a scale from 1 to 5 (1 =worst, 5= best) how well do you manage your work-life balance?</Label>
+            <Input
+              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              placeholder="I want to get very rich"
+              type="text"
+              onChange={(e) => setAns2(e.target.value)}
+            />
+        </LabelInputContainer>
+
+        <LabelInputContainer className="mb-4">
+            <Label htmlFor="email">On a scale from 1 to 5 (1 =worst, 5= best) how do you feel confident about achieving your study goal?
+</Label>
+            <Input
+              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              placeholder="I want to get very rich"
+              type="text"
+              onChange={(e) => setAns2(e.target.value)}
+            />
+        </LabelInputContainer>
+
+        <LabelInputContainer className="mb-4">
+            <Label htmlFor="email">On a scale from 1 to 5 (1 =worst, 5= best) how do you feel confident about achieving your study goal?
+</Label>
+            <Input
+              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              placeholder="I want to get very rich"
+              type="text"
+              onChange={(e) => setAns2(e.target.value)}
+            />
+        </LabelInputContainer>
 
           <button
             className="relative group/btn w-full text-white h-10 font-bold bg-[#2774AE] btn overflow-hidden rounded-xl uppercase -- before:block before:absolute before:h-full before:w-1/2 before:rounded-full before:bg-orange-400 before:top-0 before:left-1/4 before:transition-transform before:opacity-0 before:hover:opacity-100 hover:text-[#FFD100] hover:before:animate-ping transition-all duration-300 hover:-translate-y-1 transform hover:shadow-md"
