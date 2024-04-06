224e6aadeef2d885a7e0fc1541d13f88abfa62ff
Zhiyi Chen <bree_chen2005@126.com>
2024-04-06 15:28:27 +0800

    change options

--- nextjs-firebase-boilerplate/src/app/survey/page.js
+++ nextjs-firebase-boilerplate/src/app/survey/page.js
@@ -26,85 +26,90 @@ export default function Page() {
           Mental Questions
         </h2>
         <form className="my-8 bg-white rounded" onSubmit={handleSubmit}>
-          <LabelInputContainer className="mb-4">
-            <Label htmlFor="email" >Overall, on a scale 1 to 5 (1 =worst, 5= best) how would you rate your overall happiness today?
-</Label>
-</LabelInputContainer>
-            
-            <input type="radio" id="huey" name="drone" value="huey"  />
+  
+            <Label htmlFor="email" >How would you rate your overall happiness today? (1=worst, 5=best)</Label>
+            <div>
+            <input type="radio" id="huey" name="a" value="huey"  />
             <label for="huey" class="blue-text">1</label>
-            <input type="radio" id="huey" name="drone" value="huey"  />
+            <input type="radio" id="huey" name="a" value="huey"  />
             <label for="huey" class="blue-text">2</label>
-            
+            <input type="radio" id="huey" name="a" value="huey"  />
+            <label for="huey" class="blue-text">3</label>
+            <input type="radio" id="huey" name="a" value="huey"  />
+            <label for="huey" class="blue-text">4</label>
+            <input type="radio" id="huey" name="a" value="huey"  />
+            <label for="huey" class="blue-text">5</label>
+            </div>
+
+            <Label htmlFor="email">How would you rate your stress levels today? (1=worst, 5=best)</Label>
+          <form/>
+          <div>
+                <input type="radio" id="huey" name="b" value="huey"  />
+                <label for="huey" class="blue-text">1</label>
+                <input type="radio" id="huey" name="b" value="huey"  />
+                <label for="huey" class="blue-text">2</label>
+                <input type="radio" id="huey" name="b" value="huey"  />
+                <label for="huey" class="blue-text">3</label>
+                <input type="radio" id="huey" name="b" value="huey"  />
+                <label for="huey" class="blue-text">4</label>
+                <input type="radio" id="huey" name="b" value="huey"  />
+                <label for="huey" class="blue-text">5</label>
+                </div>
+
+            <Label htmlFor="password">How motivated do you feel today? (1=worst, 5=best)</Label>
+
+          <div>
+                <input type="radio" id="huey" name="c" value="huey"  />
+                <label for="huey" class="blue-text">1</label>
+                <input type="radio" id="huey" name="c" value="huey"  />
+                <label for="huey" class="blue-text">2</label>
+                <input type="radio" id="huey" name="c" value="huey"  />
+                <label for="huey" class="blue-text">3</label>
+                <input type="radio" id="huey" name="c" value="huey"  />
+                <label for="huey" class="blue-text">4</label>
+                <input type="radio" id="huey" name="c" value="huey"  />
+                <label for="huey" class="blue-text">5</label>
+                </div>
+
+
+            <Label htmlFor="email">How well do you manage your work-life balance? (1=worst, 5=best)</Label>
+            <div>
+                <input type="radio" id="huey" name="d" value="huey"  />
+                <label for="huey" class="blue-text">1</label>
+                <input type="radio" id="huey" name="d" value="huey"  />
+                <label for="huey" class="blue-text">2</label>
+                <input type="radio" id="huey" name="d" value="huey"  />
+                <label for="huey" class="blue-text">3</label>
+                <input type="radio" id="huey" name="d" value="huey"  />
+                <label for="huey" class="blue-text">4</label>
+                <input type="radio" id="huey" name="d" value="huey"  />
+                <label for="huey" class="blue-text">5</label>
+                </div>
+
+
+
+ 
+            <Label htmlFor="email">How do you feel confident about achieving your study goal? (1=worst, 5=best)
+</Label>
+         <div>
+                <input type="radio" id="huey" name="e" value="huey"  />
+                <label for="huey" class="blue-text">1</label>
+                <input type="radio" id="huey" name="e" value="huey"  />
+                <label for="huey" class="blue-text">2</label>
+                <input type="radio" id="huey" name="e" value="huey"  />
+                <label for="huey" class="blue-text">3</label>
+                <input type="radio" id="huey" name="e" value="huey"  />
+                <label for="huey" class="blue-text">4</label>
+                <input type="radio" id="huey" name="e" value="huey"  />
+                <label for="huey" class="blue-text">5</label>
+                </div>
+
           
-          <LabelInputContainer className="mb-4">
-            <Label htmlFor="email">On a scale from 1 to 5 (1 =least, 5= most) how would you rate your stress levels today?</Label>
-            <Input
-              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              placeholder="I want to get very rich"
-              type="text"
-              onChange={(e) => setAns2(e.target.value)}
-            />
-          </LabelInputContainer>
-
-
-          <label for="minmax-range" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Min-max range</label>
-          <input id="minmax-range" type="range" min="0" max="10" value="5" class="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"/>
-
-          <LabelInputContainer className="mb-4">
-            <Label htmlFor="password">On a scale from 1 to 5 (1 =least, 5= most) how motivated do you feel today?
-</Label>
-            <textarea
-              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              type="text"
-              placeholder="I love Hack with A Pro"
-              onChange={(e) => setAns3(e.target.value)}
-            />
-          </LabelInputContainer>
-          <LabelInputContainer className="mb-4">
-            <Label htmlFor="email">On a scale from 1 to 5 (1 =worst, 5= best) how well do you manage your work-life balance?</Label>
-            <Input
-              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              placeholder="I want to get very rich"
-              type="text"
-              onChange={(e) => setAns2(e.target.value)}
-            />
-          </LabelInputContainer>
-        <h2 className="font-bold text-xl text-[#2774AE] dark:text-[#2774AE]">
-          Goals Questions
-        </h2>
-
-        <LabelInputContainer className="mb-4">
-            <Label htmlFor="email">On a scale from 1 to 5 (1 =worst, 5= best) how well do you manage your work-life balance?</Label>
-            <Input
-              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              placeholder="I want to get very rich"
-              type="text"
-              onChange={(e) => setAns2(e.target.value)}
-            />
-        </LabelInputContainer>
-
-        <LabelInputContainer className="mb-4">
-            <Label htmlFor="email">On a scale from 1 to 5 (1 =worst, 5= best) how do you feel confident about achieving your study goal?
-</Label>
-            <Input
-              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              placeholder="I want to get very rich"
-              type="text"
-              onChange={(e) => setAns2(e.target.value)}
-            />
-        </LabelInputContainer>
-
-        <LabelInputContainer className="mb-4">
-            <Label htmlFor="email">On a scale from 1 to 5 (1 =worst, 5= best) how do you feel confident about achieving your study goal?
-</Label>
-            <Input
-              class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              placeholder="I want to get very rich"
-              type="text"
-              onChange={(e) => setAns2(e.target.value)}
-            />
-        </LabelInputContainer>
+        <Label htmlFor="email">Anything you wanna add?</Label>
+          <br></br>
+         
+            <textarea  class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" 
+            placeholder='I also feel myself...' name="Text1" cols="40" rows="5"></textarea>
 
           <button
             className="relative group/btn w-full text-white h-10 font-bold bg-[#2774AE] btn overflow-hidden rounded-xl uppercase -- before:block before:absolute before:h-full before:w-1/2 before:rounded-full before:bg-orange-400 before:top-0 before:left-1/4 before:transition-transform before:opacity-0 before:hover:opacity-100 hover:text-[#FFD100] hover:before:animate-ping transition-all duration-300 hover:-translate-y-1 transform hover:shadow-md"
